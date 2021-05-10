c9da8734d277065bbcee55a0c631dc13184593d1
leonhardstengel <leonhard.stengel@outlook.de>
2021-05-10 16:41:36 +0200

    Move Instance Method getCHarge

--- src/main/Rental.java
+++ src/main/Rental.java
@@ -1,7 +1,7 @@
 package main;
 
 public class Rental {
-    private Movie movie;
+    Movie movie;
     private int daysRented;
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
@@ -14,29 +14,13 @@ public class Rental {
         return movie;
     }
 
-    double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
-
     int getFrequentRenterPoints() {
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
             return 2;
         return 1;
+    }
+
+    double getCharge() {
+        return movie.getCharge(daysRented);
     }
 }
